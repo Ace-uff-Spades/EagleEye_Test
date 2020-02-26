ae0f85d446dee6788b6f479fb7689a4d1c02f235
Abhishek Kondila <abhishekkonidela@gmail.com>
2020-02-26 01:10:11 -0500

    EagleEye Testing

---
+++ EagleEye_Test/app/src/main/java/com/example/eagleeye/AcceptThread.java
@@ -0,0 +1,64 @@
+package com.example.eagleeye;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.UUID;
+
+class AcceptThread extends Thread {
+    private final BluetoothServerSocket mmServerSocket;
+    private static final String TAG = "MY_APP_DEBUG_TAG";
+    public AcceptThread(BluetoothAdapter bluetoothAdapter) {
+        // Use a temporary object that is later assigned to mmServerSocket
+        // because mmServerSocket is final.
+        BluetoothServerSocket tmp = null;
+        try {
+            // MY_UUID is the app's UUID string, also used by the client code.
+            tmp = bluetoothAdapter.listenUsingRfcommWithServiceRecord("EagleEye", UUID.fromString("EagleEye"));
+        } catch (IOException e) {
+            Log.e(TAG, "Socket's listen() method failed", e);
+        }
+        mmServerSocket = tmp;
+    }
+
+    public void run() {
+        System.out.println("IN the Accept Thread");
+        BluetoothSocket socket = null;
+        // Keep listening until exception occurs or a socket is returned.
+        while (true) {
+            try {
+                socket = mmServerSocket.accept();
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's accept() method failed", e);
+                break;
+            }
+
+            if (socket != null) {
+                // A connection was accepted. Perform work associated with
+                // the connection in a separate thread.
+                ConnectedThread connected = new ConnectedThread(socket);
+                connected.run();
+
+                try {
+                    mmServerSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
+    }
+
+    // Closes the connect socket and causes the thread to finish.
+    public void cancel() {
+        try {
+            mmServerSocket.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Could not close the connect socket", e);
+        }
+    }
+}
+
---
+++ EagleEye_Test/app/src/main/java/com/example/eagleeye/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.eagleeye;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.bluetooth.BluetoothAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final int REQUEST_ENABLE_BT = 2;
+    private TextView mTextMessage;
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch(menuItem.getItemId()){
+                        case R.id.navigation_todo:
+                            selectedFragment = new ToDoFragment();
+                            break;
+                        case R.id.navigation_done:
+                            selectedFragment = new DoneFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation_todo = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation_todo.setOnNavigationItemSelectedListener(navListener);
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            System.out.println("Bluetooth not supported on device");
+            System.out.println("Bluetooth not accepted");
+        }
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+            // Register for broadcasts when a device is discovered.
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(receiver, filter);
+
+            AcceptThread acceptThread = new AcceptThread(bluetoothAdapter);
+            acceptThread.run();
+        }
+    }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver);
+    }
+}
